1de1190383f1ee54424bc61d32fb1c05e038df37
Juan Antonio Breña Moral <bren@juanantonio.info>
2015-09-20 20:58:08 +0200

    New way to store the image https://github.com/jabrena/skymapper/issues/1

---
+++ webcam.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+
+func main() {
+	cmd := exec.Command("/usr/bin/streamer", "-c", "/dev/video0", "-o", "frame.jpeg", "-s", "1600x1200")
+	err := cmd.Run()
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("Frame stored")
+}
